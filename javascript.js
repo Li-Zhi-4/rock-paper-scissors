37054a9804ddaa4c26bf30973dcd877957f5352a
Li Zhi <s34thomp@gmail.com>
2023-03-21 23:25:11 -0400

    Added playRound() function.

--- javascript.js
+++ javascript.js
@@ -42,12 +42,44 @@ function getComputerChoice() {
     }
 }
 
+/**
+ * Returns the results of a round of Rock Paper Scissors.
+ * 
+ * @param {string} playerSelection Player's input for round.
+ * @param {string} computerSelection Computer's input for round.
+ * @returns {string} Results of a round of Rock Paper Scissors.
+ */
 function playRound(playerSelection, computerSelection) {
-    // code here
+    playerChoice = playerSelection.toLowerCase();
+
+    if (playerChoice == computerSelection) {
+        return "A tie!"
+    }
+
+    switch (computerSelection) {
+        case "rock":
+            if (playerChoice == "paper") {
+                return "You win! Paper beats Rock."
+            } else {
+                return "You lose! Rock beats Scissors."
+            }
+        case "paper":
+            if (playerChoice == "scissors") {
+                return "You win! Scissors beats Paper."
+            } else {
+                return "You lose! Paper beats Rock."
+            }
+        case "scissors":
+            if (playerChoice == "rock") {
+                return "You win! Rock beats Scissors."
+            } else {
+                return "You lose! Scissors beats Paper."
+            }
+    }
 }
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
-
-console.log(getComputerChoice());
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log("Player selected ", playerSelection);
+console.log("Computer selected ", computerSelection);
+console.log(playRound(playerSelection, computerSelection));
